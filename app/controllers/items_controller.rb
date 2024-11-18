d352389409da8ba9e91d5fdc43a23169d9e6c832
Carlota Salema <carlota.salema.98@gmail.com>
2024-11-18 14:33:55 +0000

    items controller views model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,44 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(items_params)
+    redirect_to item_path(@item)
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path, status: :see_other
+  end
+
+  private
+
+  def items_params
+    params.require(:items).permit(:name, :category, :price_per_day, :description, :user_id)
   end
 end
