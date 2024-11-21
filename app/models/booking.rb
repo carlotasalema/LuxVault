08394e202e78cb5b46dd2ec93b8ee2b49e5a2915
Carlota Salema <carlota.salema.98@gmail.com>
2024-11-21 10:42:51 +0000

    add available status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :item
   validates :start_date, :end_date, :status, presence: true
-  validates :status, acceptance: { accept: ['pending', 'accepted'] }
+  validates :status, acceptance: { accept: ['pending', 'accepted', 'available'] }
   validates :end_date, comparison: { greater_than: :start_date }
 end
