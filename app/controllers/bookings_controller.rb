ebf53635efd41a352260d6fe8dcd71b29d8fb54d
Goncalo Mendes <goncalocmendes04@gmail.com>
2024-11-22 11:26:47 +0000

    Cards in progress

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,4 @@
-class BookingsController < ApplicationController
+  lass BookingsController < ApplicationController
   def index
     @bookings = Booking.where(user_id: current_user.id)
   end
@@ -53,4 +53,3 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
-end
